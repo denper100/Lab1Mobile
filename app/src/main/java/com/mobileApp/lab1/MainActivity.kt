c5d4f5d9cad36d082ef5b8bf59be0130055815ef
denper100 <denis.pershin100@gmail.com>
2026-02-09 22:37:30 +0400

    first commit

---
+++ app/src/main/java/com/mobileApp/lab1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mobileApp.lab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.mobileApp.lab1.ui.theme.Lab1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab1Theme {
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCardScreen()
+                }
+            }
+        }
+    }
+}
